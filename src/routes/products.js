254ce38cc6f20b13dffb41c77875c23043abcdcc
PhamNhac <itk40b@gmail.com>
2021-06-05 21:02:51 +0700

    text husky

--- src/routes/products.js
+++ src/routes/products.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const productController = require('../app/controllers/ProductController');
 
-                router.get('/:slug', productController.show);
-                                            router.get('/', productController.index);
+router.get('/:slug', productController.show);
+router.get('/', productController.index);
 
 module.exports = router;
