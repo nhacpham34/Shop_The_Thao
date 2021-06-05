ae5c139f6e572f586388497c1e970444717d12db
PhamNhac <itk40b@gmail.com>
2021-06-05 20:58:25 +0700

    install husky lint-staged, prettier

--- src/routes/products.js
+++ src/routes/products.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const productController = require('../app/controllers/ProductController');
 
 router.get('/:slug', productController.show);
-                    router.get('/', productController.index);
+router.get('/', productController.index);
 
 module.exports = router;
